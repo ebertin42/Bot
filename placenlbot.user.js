393228ea6e13cadd52ae352b4718433dcbff1406
Bertin Elliot <30780477+ebertin42@users.noreply.github.com>
2022-04-03 20:45:06 +0200

    Update placenlbot.user.js

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceNL Bot
 // @namespace    https://github.com/PlaceNL/Bot
-// @version      42
+// @version      Unex
 // @description  De bot voor PlaceNL!
 // @author       NoahvdAa
 // @match        https://www.reddit.com/r/place/*
@@ -9,8 +9,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/Bilaboz/Bot/raw/master/placenlbot.user.js
-// @downloadURL  https://github.com/Bilaboz/Bot/raw/master/placenlbot.user.js
+// @updateURL    https://github.com/ebertin42/Bot/raw/master/placenlbot.user.js
+// @downloadURL  https://github.com/ebertin42/Bot/raw/master/placenlbot.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
